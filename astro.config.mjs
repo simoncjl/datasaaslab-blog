b92c2f24c32107fd8278b907f9caf882a468999b
minsri <simon.cjl@gmail.com>
2026-02-17 01:35:26 +0100

    fix: update site URL to the correct domain in configuration and layout files

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://datasaaslab-blog.pages.dev',
+  site: 'https://datasaaslab.dev',
   integrations: [mdx(), sitemap()],
   i18n: {
     locales: ['fr', 'en'],
