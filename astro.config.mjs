dfe4ebe45106b259597637fd873d87d58bb11e97
minsri <simon.cjl@gmail.com>
2026-02-17 22:27:43 +0100

    chore: migrate Astro adapter from node to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://datasaaslab.dev',
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: cloudflare(),
   integrations: [mdx(), sitemap()],
   i18n: {
     locales: ['fr', 'en'],
