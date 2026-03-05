f48ada2281fd1f2dedb7a3279db8b98ca816050a
minsri <simon.cjl@gmail.com>
2026-03-05 23:07:20 +0100

    feat: implement preferred locale redirection for blog, index, labs, and tips pages

---
+++ src/pages/labs/index.ts
@@ -0,0 +1,7 @@
+import type { APIRoute } from 'astro';
+import { preferredLocaleFromAcceptLanguage } from '../../lib/i18n';
+
+export const GET: APIRoute = ({ request }) => {
+  const preferredLocale = preferredLocaleFromAcceptLanguage(request.headers.get('accept-language'));
+  return Response.redirect(`/${preferredLocale}/labs`, 302);
+};
